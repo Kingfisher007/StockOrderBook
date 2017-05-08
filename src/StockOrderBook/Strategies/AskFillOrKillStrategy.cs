0c9cdc909d8ec95386d4b6062c145c628afa6ba0
Sushant <Sushant>
2017-05-08 23:50:20 +0530

    Added ui project

---
+++ src/StockOrderBook/Strategies/AskFillOrKillStrategy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockOrderBook.Entities;
+using StockOrderBook.Util;
+
+namespace StockOrderBook.Strategies
+{
+    class AskFillOrKillStrategy : AskTradingStrategy
+    {
+        public AskFillOrKillStrategy(OrderQueue<Bid> bid) : base(bid)
+        {
+
+        }
+
+        public override TradeExecutionResult<Ask> Execute(Ask order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/StockOrderBook/Strategies/BidFillOrKillStrategy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StockOrderBook.Entities;
+using StockOrderBook.Util;
+
+namespace StockOrderBook.Strategies
+{
+    class BidFillOrKillStrategy : BidTradingStrategy
+    {
+        public BidFillOrKillStrategy(OrderQueue<Ask> ask) : base(ask)
+        {
+
+        }
+
+        public override TradeExecutionResult<Bid> Execute(Bid order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
