1f1ec01834067c546bb586f8f96288d783024419
Sushant Mhetre <sushant.mhetre@hotmail.com>
2018-09-17 01:32:35 +0530

    big fixing

--- src/StockOrderBook/Strategies/Bid/BidIOCStrategy.cs
+++ src/StockOrderBook/Strategies/Bid/BidIOCStrategy.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using EOrderBook.Entities;
 using StockOrderBook.Util;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace StockOrderBook.Strategies
 {
-    public class BidIOCStrategy : BidTradingStrategy
+    internal class BidIOCStrategy : BidTradingStrategy
     {
         public BidIOCStrategy(IOrderBook orderbook, IFillBook tradebook) : base(orderbook, tradebook)
         {
@@ -15,65 +13,7 @@ namespace StockOrderBook.Strategies
 
         protected override TradeExecutionResult ExecuteBid(Bid order)
         {
-            ValidateParams(order);
-
-			int cumVolume = 0;
-			TradeResult result = TradeResult.NotTraded;
-			Stack<Ask> matchedOrders = new Stack<Ask>();
-
-			// Accumulate Ask orders to fill bid order 
-			foreach(Ask ask in Asks.Orders)
-			{
-				if (ask.AskPrice > order.BidPrice)
-				{
-					break;
-				}
-
-				matchedOrders.Push(ask);
-				cumVolume += ask.Volume;
-
-				if (cumVolume == order.Volume)
-				{
-					break;
-				}
-
-				if (cumVolume > order.Volume)
-				{
-					// if cum volume exceeds bid volume, try to divide last ask order to match volumes
-					matchedOrders.Pop();
-					cumVolume -= ask.Volume;
-
-					if (ask.Trade != TradeType.AllOrNothing)
-					{
-						ask.TradedVolume = ask.Volume - (order.Volume - cumVolume);
-
-						matchedOrders.Push(ask);
-						cumVolume += ask.TradedVolume;
-						result = TradeResult.Traded;
-						break;
-					}
-					// IOC allows partial fill
-					else
-					{
-						order.TradedVolume = cumVolume;
-						result = TradeResult.Traded;
-						break;
-					}
-				}
-			}
-			// execute trade
-			if (result == TradeResult.Traded)
-			{
-				// trades
-				AddTrades(CreateTrades(order, matchedOrders));
-				Asks.Remove(matchedOrders.ToList());
-				result = TradeResult.Traded;
-			}
-
-			// remove order since its IOC
-			Bids.Remove(order);
-
-			return new TradeExecutionResult(result, order.BidPrice, matchedOrders.Last().AskPrice);
+            throw new NotImplementedException();
         }
     }
 }
